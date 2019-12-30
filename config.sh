279b2d04db85a3fc9a830a510b284c6eb84e7572
Ariel Ril <arielril@gmail.com>
2019-12-30 08:50:49 -0300

    add plugin install to vim

--- config.sh
+++ config.sh
@@ -34,8 +34,8 @@ set -g __done_min_cmd_duration 120000;
 # cabal install postgresql-simple
 
 # Vim
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# vim +PluginInstall +qall
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 # (cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer)
 
 echo '-----------'
