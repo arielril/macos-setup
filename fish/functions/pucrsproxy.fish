1b70160a567e73cccce715c6a3d60434f588c6a9
Ariel Ril <arielril@gmail.com>
2024-04-17 14:47:37 -0300

    Update pucrsproxy.fish

--- fish/functions/pucrsproxy.fish
+++ fish/functions/pucrsproxy.fish
@@ -5,11 +5,9 @@ function pucrsproxy
 
   if test $on_off = 'on'
     networksetup -setautoproxyurl 'Wi-Fi' $proxy_url
-    networksetup -setpassiveftp 'Wi-Fi' off
     set_color green; echo 'Proxy ON'
   else
     networksetup -setautoproxyurl 'Wi-Fi' ' '
-    networksetup -setpassiveftp 'Wi-Fi' off
     set_color red; echo 'Proxy OFF'
   end
 end
